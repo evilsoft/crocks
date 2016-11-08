d3243f71ab22d08883ac402a6d5a67d51b340e9e
Ian Hofmann-Hicks <ihofmannhicks@articulate.com>
2016-11-07 20:15:41 -0800

    add contra and promap pointfree to crocks

---
+++ pointfree/contramap.js
@@ -0,0 +1,24 @@
+/** @license ISC License (c) copyright 2016 original and current authors */
+/** @author Ian Hofmann-Hicks (evil) */
+
+const curry = require('../funcs/curry')
+const composeB = require('../combinators/composeB')
+const isFunction = require('../internal/isFunction')
+
+// contramap : Functor f => (b -> a) -> f b -> f a
+function contramap(fn, m) {
+  if(!isFunction(fn)) {
+    throw new TypeError('contramap: Function required for first argument')
+  }
+  else if(isFunction(m)) {
+    return composeB(m, fn)
+  }
+  else if(m && isFunction(m.contramap)) {
+    return m.contramap(fn)
+  }
+  else {
+    throw new TypeError('contramap: Function or Contavariant Functor of the same type required for second requirement')
+  }
+}
+
+module.exports = curry(contramap)
---
+++ pointfree/promap.spec.js
@@ -0,0 +1,78 @@
+const test = require('tape')
+const sinon = require('sinon')
+const helpers = require('../test/helpers')
+
+const noop = helpers.noop
+const bindFunc = helpers.bindFunc
+const isFunction = require('../internal/isFunction')
+
+const identity = require('../combinators/identity')
+
+const promap = require('./promap')
+
+test('promap pointfree', t => {
+  const m = bindFunc(promap)
+  const f = { promap: noop }
+
+  t.ok(isFunction(promap), 'is a function')
+
+  t.throws(m(undefined, noop, f), 'throws if first arg is undefined')
+  t.throws(m(null, noop, f), 'throws if first arg is null')
+  t.throws(m(0, noop, f), 'throws if first arg is a falsey number')
+  t.throws(m(1, noop, f), 'throws if first arg is a truthy number')
+  t.throws(m('', noop, f), 'throws if first arg is a falsey string')
+  t.throws(m('string', noop, f), 'throws if first arg is a truthy string')
+  t.throws(m(false, noop, f), 'throws if first arg is false')
+  t.throws(m(true, noop, f), 'throws if first arg is true')
+  t.throws(m([], noop, f), 'throws if first arg is an array')
+  t.throws(m({}, noop, f), 'throws if first arg is an object')
+
+  t.throws(m(noop, undefined, f), 'throws if second arg is undefined')
+  t.throws(m(noop, null, f), 'throws if second arg is null')
+  t.throws(m(noop, 0, f), 'throws if second arg is a falsey number')
+  t.throws(m(noop, 1, f), 'throws if second arg is a truthy number')
+  t.throws(m(noop, '', f), 'throws if second arg is a falsey string')
+  t.throws(m(noop, 'string', f), 'throws if second arg is a truthy string')
+  t.throws(m(noop, false, f), 'throws if second arg is false')
+  t.throws(m(noop, true, f), 'throws if second arg is true')
+  t.throws(m(noop, [], f), 'throws if second arg is an array')
+  t.throws(m(noop, {}, f), 'throws if second arg is an object')
+
+  t.throws(m(noop, noop, undefined), 'throws if third arg is undefined')
+  t.throws(m(noop, noop, null), 'throws if third arg is null')
+  t.throws(m(noop, noop, 0), 'throws if third arg is a falsey number')
+  t.throws(m(noop, noop, 1), 'throws if third arg is a truthy number')
+  t.throws(m(noop, noop, ''), 'throws if third arg is a falsey string')
+  t.throws(m(noop, noop, 'string'), 'throws if third arg is a truthy string')
+  t.throws(m(noop, noop, false), 'throws if third arg is false')
+  t.throws(m(noop, noop, true), 'throws if third arg is true')
+  t.throws(m(noop, noop, {}), 'throws if third arg is an object')
+
+  t.end()
+})
+
+test('promap profunctor', t => {
+  const m = { promap: sinon.spy(noop) }
+
+  promap(identity)(identity, m)
+
+  t.ok(m.promap.calledWith(identity, identity), 'calls promap on profunctor, passing the function')
+
+  t.end()
+})
+
+test('promap function composition', t => {
+  const left = sinon.spy(x => x + 2)
+  const right = sinon.spy(x => x * 10)
+
+  const comp = promap(left, right, identity)
+  const result = comp(5)
+
+  t.ok(isFunction(comp), 'returns a function')
+  t.ok(left.calledBefore(right), 'calls the left function first')
+
+  t.ok(right.calledWith(left.returnValues[0]), 'result of left is passed to the right')
+  t.equal(result, right.returnValues[0], 'result of right is returned')
+
+  t.end()
+})

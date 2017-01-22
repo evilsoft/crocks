c7c4fbd40427a1d5f2f24d82a4b21ff0df3e67c9
Ian Hofmann-Hicks <ihofmannhicks@articulate.com>
2017-01-22 11:58:41 -0800

    update copywrite for 2017 files

--- predicates/isBoolean.js
+++ predicates/isBoolean.js
@@ -1,4 +1,4 @@
-/** @license ISC License (c) copyright 2016 original and current authors */
+/** @license ISC License (c) copyright 2017 original and current authors */
 /** @author Ian Hofmann-Hicks (evil) */
 
 // isBoolean : a -> Boolean

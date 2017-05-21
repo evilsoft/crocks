05adc046d9fddf19318d59b9b81e8ba3d9647782
Matt Ross <amsross@gmail.com>
2017-05-21 11:23:53 -0400

    More specific errors in fork

--- pointfree/fork.js
+++ pointfree/fork.js
@@ -7,11 +7,11 @@ const curry = require('../helpers/curry')
 const Async = require('../crocks/Async')
 
 function fork(reject, resolve, m) {
-  if(!isFunction(reject) || !isFunction(resolve)) {
-    throw new TypeError('fork: Reject and resolve functions required')
+  if(!(isFunction(reject) && isFunction(resolve))) {
+    throw new TypeError('fork: Reject and resolve functions required for first two arguments')
   }
   if(!(m && isSameType(Async, m))) {
-    throw new TypeError('fork: Async required')
+    throw new TypeError('fork: Async required for third argument')
   }
 
   m.fork(reject, resolve)

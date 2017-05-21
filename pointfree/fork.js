549312e693d26b5f8cdbfed503bc67377dc6a5d2
Matt Ross <amsross@gmail.com>
2017-05-21 12:08:48 -0400

    Update pointfree fork to accept a cancel
    RE: #124

--- pointfree/fork.js
+++ pointfree/fork.js
@@ -6,15 +6,18 @@ const isSameType = require('../predicates/isSameType')
 const curry = require('../helpers/curry')
 const Async = require('../crocks/Async')
 
-function fork(reject, resolve, m) {
+function fork(reject, resolve, cancel, m) {
   if(!(isFunction(reject) && isFunction(resolve))) {
     throw new TypeError('fork: Reject and resolve functions required for first two arguments')
   }
+  if(cancel && !isFunction(cancel)) {
+    throw new TypeError('fork: Function or falsey value required for third argument')
+  }
   if(!(m && isSameType(Async, m))) {
-    throw new TypeError('fork: Async required for third argument')
+    throw new TypeError('fork: Async required for fourth argument')
   }
 
-  m.fork(reject, resolve)
+  m.fork(reject, resolve, cancel)
 }
 
 module.exports = curry(fork)

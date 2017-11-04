26ae7a153e041f2be0d85a9b13d0e75fd6624ed8
Ian Hofmann-Hicks <ihofmannhicks@articulate.com>
2017-11-04 16:15:53 -0700

    remove value from `Arrow`

--- src/Arrow/index.js
+++ src/Arrow/index.js
@@ -19,11 +19,8 @@ function Arrow(runWith) {
     throw new TypeError('Arrow: Function required')
   }
 
-  const value =
-    () => runWith
-
   const inspect =
-    () => `Arrow${_inspect(value())}`
+    () => `Arrow${_inspect(runWith)}`
 
   const id =
     _id
@@ -89,7 +86,7 @@ function Arrow(runWith) {
   }
 
   return {
-    inspect, type, value, runWith,
+    inspect, type, runWith,
     id, compose, map, contramap,
     promap, first, second, both
   }

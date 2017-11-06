6d695e4c0eef99a34e7c912bc231a4f9b4300e19
Ian Hofmann-Hicks (evil) <evilsoft@aol.com>
2017-11-05 17:33:45 -0800

    have `read` return a `Pair` for `Writer`

--- src/Writer/index.js
+++ src/Writer/index.js
@@ -4,6 +4,7 @@
 const _implements = require('../core/implements')
 const _inspect = require('../core/inspect')
 const __type = require('../core/types').type('Writer')
+const Pair = require('../core/Pair')
 
 const isFunction = require('../core/isFunction')
 const isMonoid = require('../core/isMonoid')
@@ -45,10 +46,8 @@ function _Writer(Monoid) {
     const inspect =
       constant(`Writer(${_inspect(log())}${_inspect(value())} )`)
 
-    const read = constant({
-      log: log().value(),
-      value: value()
-    })
+    const read = () =>
+      Pair(log(), val)
 
     function map(fn) {
       if(!isFunction(fn)) {

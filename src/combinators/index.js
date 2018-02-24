25318da82281ead59f6bace7db49adc5b1e30e35
Dale Francis <dalefrancis88@gmail.com>
2018-02-24 17:06:06 +1100

    Fixed indentations

--- src/combinators/index.js
+++ src/combinators/index.js
@@ -1,8 +1,8 @@
 module.exports = {
-    applyTo: require('./applyTo'),
-    composeB: require('./composeB'),
-    constant: require('./constant'),
-    flip: require('./flip'),
-    identity: require('./identity'),
-    substitution: require('./substitution')
+  applyTo: require('./applyTo'),
+  composeB: require('./composeB'),
+  constant: require('./constant'),
+  flip: require('./flip'),
+  identity: require('./identity'),
+  substitution: require('./substitution')
 }
--- src/logic/index.js
+++ src/logic/index.js
@@ -1,8 +1,8 @@
 module.exports = {
-    and: require('./and'),
-    ifElse: require('./ifElse'),
-    not: require('./not'),
-    or: require('./or'),
-    unless: require('./unless'),
-    when: require('./when')
+  and: require('./and'),
+  ifElse: require('./ifElse'),
+  not: require('./not'),
+  or: require('./or'),
+  unless: require('./unless'),
+  when: require('./when')
 }

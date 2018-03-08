f4d1985cad81b4f91b212f0de7fb4e394914686c
Dale Francis <dalefrancis88@gmail.com>
2018-03-09 08:19:45 +1100

    Adding in more tests for find

--- src/Maybe/find.spec.js
+++ src/Maybe/find.spec.js
@@ -12,15 +12,27 @@ test('find is protected', t => {
   const err = /find: Function required for first argument/
 
   t.throws(fn(undefined), err, 'throws if foldable is undefined')
+  t.throws(fn(null), err, 'throws if foldable is null')
+  t.throws(fn(0), err, 'throws if foldable is falsey')
+  t.throws(fn(1), err, 'throws if foldable is truthy')
+  t.throws(fn(''), err, 'throws if foldable is falsey string')
+  t.throws(fn('string'), err, 'throws if foldable is truthy string')
+  t.throws(fn(false), err, 'throws if arg is false')
+  t.throws(fn(true), err, 'throws if arg is true')
 
   t.end()
 })
 
-test('find', t => {
+test('find works as expected', t => {
   t.ok(isFunction(find), 'is a function')
 
-  t.ok(isFunction(find(() => true, undefined).either), 'returns a Maybe for undefined value')
   t.ok(isFunction(find(() => true, []).either), 'returns a Maybe for valid value')
+
+  t.equal(find(() => true, undefined).either(() => 'Nothing', x => x), 'Nothing', 'returns a Nothing for undefined value')
+  t.equal(find(x => x > 3, [ 1, 2, 3, 4, 5, 6 ]).either(() => 'Nothing', x => x), 4, 'returns the correct value')
+
+  t.equal(find(x => x > 3, [ 0, null, undefined, 4, 5, 6 ]).either(() => 'Nothing', x => x), 4, 'handles bad values in foldable')
+  t.equal(find(x => x > 6, [ 1, 2, 3, 4, 5, 6 ]).either(() => 'Nothing', x => x), 'Nothing', 'returns nothing when value is not found')
 
   t.end()
 })

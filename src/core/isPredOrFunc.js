4a3b8b0c7f3e0d81287237c22cd7aab10da9f079
Dale Francis <dalefrancis88@gmail.com>
2018-03-11 09:32:24 +1100

    IsPredOrFunc updates, more tests

--- src/core/isPredOrFunc.js
+++ src/core/isPredOrFunc.js
@@ -1,8 +1,12 @@
 /** @license ISC License (c) copyright 2018 original and current authors */
 /** @author Dale Francis (dalefrancis88) */
 
-const isPredicate = require('./isPredicate')
-const isFunction = require(isFunction)
-const compose = require('./compose')
+const Pred = require('./types').proxy('Pred')
+const isFunction = require('./isFunction')
+const isSameType = require('./isSameType')
 
-module.exports = compose(isFunction, isPredicate)
+// isPredOrFunc :: (func | pred) -> bool
+const isPredOrFunc = predOrFunc =>
+  !!predOrFunc && (isFunction(predOrFunc) || isSameType(Pred, predOrFunc))
+
+module.exports = isPredOrFunc
---
+++ src/core/isPredOrFunc.spec.js
@@ -0,0 +1,30 @@
+const test = require('tape')
+
+const Pred = require('../Pred')
+const isNumber = require('./isNumber')
+const unit = require('./_unit')
+const isLargeNumber =
+  Pred(isNumber)
+    .concat(Pred(x => x > 100))
+
+const isPredOrFunc = require('./isPredOrFunc')
+
+test('isPredOrFunc core', t => {
+  t.equal(typeof isPredOrFunc, 'function', 'is a function')
+
+  t.equal(isPredOrFunc(unit), true, 'returns true when passed a function')
+  t.equal(isPredOrFunc(isLargeNumber), true, 'returns true when passed an predicate')
+
+  t.equal(isPredOrFunc(undefined), false, 'returns false when passed undefined')
+  t.equal(isPredOrFunc(null), false, 'returns false when passed null')
+  t.equal(isPredOrFunc(0), false, 'returns false when passed a falsey number')
+  t.equal(isPredOrFunc(1), false, 'returns false when passed a truthy number')
+  t.equal(isPredOrFunc(''), false, 'returns false when passed a falsey string')
+  t.equal(isPredOrFunc('string'), false, 'returns false when passed a truthy string')
+  t.equal(isPredOrFunc(false), false, 'returns false when passed false')
+  t.equal(isPredOrFunc(true), false, 'returns false when passed true')
+  t.equal(isPredOrFunc([]), false, 'returns false when passed an array')
+  t.equal(isPredOrFunc({}), false, 'returns false when passed an object')
+
+  t.end()
+})

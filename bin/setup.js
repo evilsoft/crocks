a648f26339a081cb33455274d96f16582395ef58
Dale Francis <dalefrancis88@gmail.com>
2018-03-13 12:21:51 +1100

    Removed clear, removed redundant check, added no-save

--- bin/setup.js
+++ bin/setup.js
@@ -9,17 +9,13 @@ var root = resolve(__dirname, '..')
 var docs = resolve(__dirname, '..', 'docs')
 var isWindows = os.platform().startsWith('win')
 var npmCmd = isWindows ? 'npm.cmd' : 'npm'
-var rmrf = isWindows ? 'if exist "node_modules" (rd /s /q node_modules)' : 'rm -rf node_modules'
-
-cp.execSync('cls', { env: process.env, stdio: 'inherit' })
+var rmrf = isWindows ? 'rd /s /q node_modules' : 'rm -rf node_modules'
 
 install(root)
 install(docs)
 
 function install(dir) {
   console.log('working on ' + dir)
-
-  if (!fs.existsSync(join(dir, 'package.json'))) { return }
 
   if (fs.existsSync(join(dir, 'node_modules'))) {
     console.log('Found a node_modules folder exists, deleting...')
@@ -28,5 +24,5 @@ function install(dir) {
   }
 
   console.log('Running npm install...')
-  cp.spawnSync(npmCmd, [ 'i' ], { env: process.env, cwd: dir, stdio: 'inherit' })
+  cp.spawnSync(npmCmd, [ 'i', '--no-save' ], { env: process.env, cwd: dir, stdio: 'inherit' })
 }

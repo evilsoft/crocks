536842186e827856583194fc3dbad03bdd3ff8c4
Ian Hofmann-Hicks <evilsoft@aol.com>
2018-06-25 18:50:40 -0700

    curry constant (#283)

--- src/combinators/constant.js
+++ src/combinators/constant.js
@@ -1,9 +1,11 @@
 /** @license ISC License (c) copyright 2016 original and current authors */
 /** @author Ian Hofmann-Hicks (evil) */
 
+const curry = require('../core/curry')
+
 // Constant (Kestrel)
 // constant : a -> b -> a
 const constant =
   x => () => x
 
-module.exports = constant
+module.exports = curry(constant)

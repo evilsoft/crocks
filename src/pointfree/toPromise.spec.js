5be73d7d36ba0ed47476de5925befcb3a1561add
Dale Francis <dalefrancis88@gmail.com>
2018-07-22 20:46:44 +1000

    Added negative tests for function check

--- src/pointfree/toPromise.spec.js
+++ src/pointfree/toPromise.spec.js
@@ -1,4 +1,7 @@
 const test = require('tape')
+const helpers = require('../test/helpers')
+
+const bindFunc = helpers.bindFunc
 
 const isFunction  = require('../core/isFunction')
 
@@ -6,17 +9,31 @@ const Async = require('../Async')
 const toPromise = require('./toPromise')
 
 test('toPromise pointfree', t => {
-  t.plan(3)
+  t.plan(13)
 
-  const val = 1337
+  const f = bindFunc(toPromise)
+  const result = 1337
+
+  const err = /toPromise: Async required/
+
+  t.throws(f(undefined), err, 'throws if passed undefined')
+  t.throws(f(null), err, 'throws if passed null')
+  t.throws(f(0), err, 'throws if passed a falsey number')
+  t.throws(f(1), err, 'throws if passed a truthy number')
+  t.throws(f(''), err, 'throws if passed a falsey string')
+  t.throws(f('string'), err, 'throws if passed a truthy string')
+  t.throws(f(false), err, 'throws if passed false')
+  t.throws(f(true), err, 'throws if passed true')
+  t.throws(f([]), err, 'throws if passed an array')
+  t.throws(f({}), err, 'throws if passed an object')
 
   const rej = y => x => t.equal(x, y, 'rejects a rejected Async')
   const res = y => x => t.equal(x, y, 'resolves a resolved Async')
 
-  const testFunc = a => toPromise(a).then(res(val)).catch(rej(val))
+  const testFunc = a => toPromise(a).then(res(result)).catch(rej(result))
 
   t.ok(isFunction(toPromise), 'is a function')
 
-  testFunc(Async.Rejected(val))
-  testFunc(Async.Resolved(val))
+  testFunc(Async.Rejected(result))
+  testFunc(Async.Resolved(result))
 })

7d6cc4a6db2ac9bc7ee8a21a38a5c715731b1a09
Ian Hofmann-Hicks <ihofmannhicks@articulate.com>
2018-08-18 22:39:09 -0700

    update copywrite date

--- src/pointfree/foldMap.js
+++ src/pointfree/foldMap.js
@@ -1,4 +1,4 @@
-/** @license ISC License (c) copyright 2017 original and current authors */
+/** @license ISC License (c) copyright 2018 original and current authors */
 /** @author Ian Hofmann-Hicks (evil) */
 
 const _array = require('../core/array')

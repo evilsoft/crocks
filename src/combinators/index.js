73e2034fc7eadfa3ff53ddea7b5926e1a5e5f1fe
Matt Ross <amsross@gmail.com>
2018-10-25 16:45:23 -0400

    Move `converge` to the correct `combinators` directory and expose

--- src/combinators/index.js
+++ src/combinators/index.js
@@ -2,6 +2,7 @@ module.exports = {
   applyTo: require('./applyTo'),
   composeB: require('./composeB'),
   constant: require('./constant'),
+  converge: require('./converge'),
   flip: require('./flip'),
   identity: require('./identity'),
   substitution: require('./substitution')

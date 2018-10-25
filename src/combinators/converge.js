4f2bd3ad135e395b9557b12e889f98acc5d78eba
Matt Ross <amsross@gmail.com>
2018-10-25 18:03:27 -0400

    Update helper paths, error message, license date, error checks

--- src/combinators/converge.js
+++ src/combinators/converge.js
@@ -1,14 +1,14 @@
-/** @license ISC License (c) copyright 2016 original and current authors */
+/** @license ISC License (c) copyright 2018 original and current authors */
 /** @author Matt Ross (amsross) */
 
-const curry = require('../helpers/curry')
-const isFunction = require('../predicates/isFunction')
+const curry = require('../core/curry')
+const isFunction = require('../core/isFunction')
 
 // converge (Phoenix or Starling Prime)
 // (b -> c -> d) -> (a -> b) -> (a -> c) -> a -> d
 function converge(f, g, h, x) {
   if(!isFunction(f) || !isFunction(g) || !isFunction(h)) {
-    throw new TypeError('converge: Functions required for first two arguments')
+    throw new TypeError('converge: Functions required for first three arguments')
   }
 
   return curry(f)(g(x), h(x))

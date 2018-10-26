2411918f5ed07cd76ac6247937c14aa8f5e72ae9
Dale Francis <dalefrancis88@gmail.com>
2018-10-25 22:52:34 -0700

    Adding Kleisli support on asyncToPromise

--- src/Async/asyncToPromise.js
+++ src/Async/asyncToPromise.js
@@ -1,17 +1,26 @@
 /** @license ISC License (c) copyright 2018 original and current authors */
 /** @author Dale Francis */
 
+const curry = require('../core/curry')
 const isSameType = require('../core/isSameType')
+const isFunction = require('../core/isFunction')
 
 const Async = require('../core/types').proxy('Async')
 
-// Async e a -> Promise a e
-function asyncToPromise(m) {
+const toPromise = m => {
   if(!isSameType(Async, m)) {
-    throw new TypeError('asyncToPromise: Async required')
+    throw new TypeError('asyncToPromise: Async or a function returning an Async required')
   }
 
   return m.toPromise()
 }
 
-module.exports = asyncToPromise
+// asyncToPromise :: Async e a -> Promise a e
+// asyncToPromise :: (a -> Async e b) -> a -> Promise b e
+function asyncToPromise(async) {
+  return isFunction(async) ?
+    x => toPromise(async(x)) :
+    toPromise(async)
+}
+
+module.exports = curry(asyncToPromise)
--- src/Async/asyncToPromise.spec.js
+++ src/Async/asyncToPromise.spec.js
@@ -6,16 +6,20 @@ const bindFunc = helpers.bindFunc
 
 const Async = require('.')
 const isFunction  = require('../core/isFunction')
+const isPromise  = require('../core/isPromise')
 
 const unit = () => undefined
+const identity = x => x
 
 const asyncToPromise = require('./asyncToPromise')
 
-test('asyncToPromise pointfree', t => {
+test('asyncToPromise transform', t => {
   const f = bindFunc(asyncToPromise)
   const m = Async(unit)
 
-  const err = /asyncToPromise: Async required/
+  const err = /asyncToPromise: Async or a function returning an Async required/
+
+  t.ok(isFunction(asyncToPromise), 'is a function')
 
   t.throws(f(undefined), err, 'throws if passed undefined')
   t.throws(f(null), err, 'throws if passed null')
@@ -36,6 +40,48 @@ test('asyncToPromise pointfree', t => {
 
   t.ok(m.toPromise.called, 'calls asyncToPromise on the passed in container')
   t.equal(result, unit(), 'returns the result of calling asyncToPromise')
+
+  t.end()
+})
+
+test('asyncToPromise with Async', t => {
+  const data = 'TestData'
+  const async = Async.of(data)
+
+  const p = asyncToPromise(async)
+
+  const rej = y => x => t.equal(x, y, 'rejects a rejected Promise')
+  const res = y => x => t.equal(x, y, 'resolves a resolved Promise')
+
+  t.ok(isPromise(p), 'returns a promise from Async')
+  p.then(res(data)).catch(rej(data))
+
+  t.end()
+})
+
+test('asyncToPromise with Async returning function', t => {
+  const f = bindFunc(asyncToPromise(identity))
+
+  t.throws(f(undefined), TypeError, 'throws if function returns undefined')
+  t.throws(f(null), TypeError, 'throws if function returns null')
+  t.throws(f(0), TypeError, 'throws if function returns a falsey number')
+  t.throws(f(1), TypeError, 'throws if function returns a truthy number')
+  t.throws(f(''), TypeError, 'throws if function returns a falsey string')
+  t.throws(f('string'), TypeError, 'throws if function returns a truthy string')
+  t.throws(f(false), TypeError, 'throws if function returns false')
+  t.throws(f(true), TypeError, 'throws if function returns true')
+  t.throws(f({}), TypeError, 'throws if function returns an object')
+  t.throws(f([]), TypeError, 'throws if function returns an array')
+
+  const data = 'TestData'
+  const promise = Async.of(data)
+
+  const m = asyncToPromise(identity, promise)
+  const rej = y => x => t.equal(x, y, 'rejects a rejected Promise')
+  const res = y => x => t.equal(x, y, 'preserves the structure of underlying data')
+
+  t.ok(isPromise(m), 'returns a promise from a function returning a Async')
+  m.then(res(data)).catch(rej(data))
 
   t.end()
 })

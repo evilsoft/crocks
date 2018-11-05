ef57b4871fc42d7a2c258f6a5e3afc7b385e2487
Ian Hofmann-Hicks <evilsoft@aol.com>
2018-11-05 10:16:40 -0800

    better compat with `isNil`

--- src/core/isNil.js
+++ src/core/isNil.js
@@ -1,11 +1,11 @@
 /** @license ISC License (c) copyright 2017 original and current authors */
 /** @author Ian Hofmann-Hicks (evil) */
 
+/* eslint eqeqeq: "off" */
+
 // isNil : a -> Boolean
 function isNil(x) {
-  return x === undefined
-    || x === null
-    || Number.isNaN(x)
+  return x == null || x !== x
 }
 
 module.exports = isNil

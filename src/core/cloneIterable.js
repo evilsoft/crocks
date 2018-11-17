672f071272a98415337462c155372c1239dc24cc
HenriqueLimas <henrique.ramos.limas@gmail.com>
2018-11-17 12:47:07 +0100

    Remove array/string checks

--- src/core/cloneIterable.js
+++ src/core/cloneIterable.js
@@ -1,18 +1,7 @@
 /** @license ISC License (c) copyright 2018 original and current authors */
 /** @author Henrique Limas (HenriqueLimas) */
 
-const isArray = require('./isArray')
-const isObject = require('./isObject')
-
 function cloneIterable(source) {
-  if (isArray(source)) {
-    return [].concat(source)
-  }
-
-  if (!isObject(source)) {
-    return source
-  }
-
   let copy = Object.create(Object.getPrototypeOf(source))
   Object.assign(copy, source)
 
--- src/core/cloneIterable.spec.js
+++ src/core/cloneIterable.spec.js
@@ -2,33 +2,6 @@ const test = require('tape')
 
 const cloneIterable = require('./cloneIterable')
 
-test('cloneIterable arrays', t => {
-  const empty = []
-  const one = [ 1 ]
-  const two = [ 2, 3 ]
-
-  t.deepEqual(cloneIterable(empty), empty, 'returns a clone on empty array')
-  t.notEqual(cloneIterable(empty), empty, 'returns not the same reference on empty array')
-  t.deepEqual(cloneIterable(one), one, 'returns a clone on one element array')
-  t.notEqual(cloneIterable(one), one, 'returns not the same reference on one element array')
-  t.deepEqual(cloneIterable(two), two, 'returns a clone on one element array')
-  t.notEqual(cloneIterable(two), two, 'returns not the same reference on two elements array')
-
-  t.end()
-})
-
-test('cloneIterable string', t => {
-  const empty = ''
-  const one = 'a'
-  const two = 'bc'
-
-  t.equal(cloneIterable(empty), empty, 'returns a copy of the empty string')
-  t.equal(cloneIterable(one), one, 'returns a copy of the one char string')
-  t.equal(cloneIterable(two), two, 'returns a copy of the two char string')
-
-  t.end()
-})
-
 test('cloneIterable shapes', t => {
   function First() { this.x = 42 }
   First.prototype.value = function() {

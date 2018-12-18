f023775cf2e9933095a0f3c4776f4a9029ed7ab8
Dale Francis <dalefrancis88@gmail.com>
2018-12-17 22:47:21 -0800

    Updating implementation

--- src/core/isDate.js
+++ src/core/isDate.js
@@ -1,11 +1,10 @@
 /** @license ISC License (c) copyright 2018 original and current authors */
 /** @author Dale Francis (dalefrancis88) */
 
-const hasAlg = require('./hasAlg')
-
 // isDate : a -> Boolean
 function isDate(x) {
-  return hasAlg('valueOf', x) && x instanceof Date && !isNaN(x.valueOf())
+  return Object.prototype.toString.apply(x) === '[object Date]'
+    && !isNaN(x.valueOf())
 }
 
 module.exports = isDate

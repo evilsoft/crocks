c138c566e795bfe5197d00eeed41c4f22c553407
Dale Francis <dalefrancis88@gmail.com>
2018-12-23 10:55:10 -0800

    Adding in new isDate function (#362)
    * Adding in new isDate function
    
    * Updating implementation

---
+++ src/core/isDate.spec.js
@@ -0,0 +1,28 @@
+const test = require('tape')
+
+const isFunction = require('./isFunction')
+
+const identity = x => x
+
+const isDate = require('./isDate')
+
+test('isDate core', t => {
+  t.ok(isFunction(isDate))
+
+  t.equal(isDate(undefined), false, 'returns false for undefined')
+  t.equal(isDate(null), false, 'returns false for null')
+  t.equal(isDate(0), false, 'returns false for falsey number')
+  t.equal(isDate(1), false, 'returns false for truthy number')
+  t.equal(isDate(false), false, 'returns false for false')
+  t.equal(isDate(true), false, 'returns false for true')
+  t.equal(isDate({}), false, 'returns false for an object')
+  t.equal(isDate([]), false, 'returns false for an array')
+  t.equal(isDate(identity), false, 'returns false for function')
+
+  t.equal(isDate(new Date(undefined)), false, 'returns false for invalid date')
+
+  t.equal(isDate(new Date()), true, 'returns true for new Date')
+  t.equal(isDate(new Date('2018/12')), true, 'returns true from parsed date')
+
+  t.end()
+})
---
+++ src/predicates/isDate.js
@@ -0,0 +1,5 @@
+/** @license ISC License (c) copyright 2018 original and current authors */
+/** @author Dale Francis (dalefrancis88) */
+
+module.exports =
+  require('../core/isDate')

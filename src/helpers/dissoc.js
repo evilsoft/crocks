b5712e8183b55257f9bdce4ba122fb283d8690a6
Ian Hofmann-Hicks <evilsoft@aol.com>
2019-03-30 14:30:04 -0700

    add unsetProp helpler and keep dissoc around (#386)

--- src/helpers/dissoc.js
+++ src/helpers/dissoc.js
@@ -1,30 +1,7 @@
 /** @license ISC License (c) copyright 2017 original and current authors */
 /** @author Ian Hofmann-Hicks (evil) */
 
-const curry = require('../core/curry')
-const isString = require('../core/isString')
-const isObject  = require('../core/isObject')
+const unsetProp = require('./unsetProp')
 
-function applyDissoc(key, obj) {
-  return function(acc, k) {
-    if(obj[k] !== undefined && k !== key) {
-      acc[k] = obj[k]
-    }
-    return acc
-  }
-}
-
-// dissoc : String -> Object -> Object
-function dissoc(key, obj) {
-  if(!isString(key)) {
-    throw new TypeError('dissoc: String required for first argument')
-  }
-
-  if(!isObject(obj)) {
-    throw new TypeError('dissoc: Object required for second argument')
-  }
-
-  return Object.keys(obj).reduce(applyDissoc(key, obj), {})
-}
-
-module.exports = curry(dissoc)
+module.exports =
+  unsetProp.origFn('dissoc')
--- src/helpers/dissoc.spec.js
+++ src/helpers/dissoc.spec.js
@@ -2,41 +2,91 @@ const test = require('tape')
 const helpers = require('../test/helpers')
 
 const bindFunc = helpers.bindFunc
-const unit = require('../core/_unit')
+
+const isFunction = require('../core/isFunction')
+const unit =  Function.prototype
 
 const dissoc = require('./dissoc')
 
-test('dissoc', t => {
+const err =
+  /dissoc: Non-empty String required or Positive Integer required for first argument/
+
+test('dissoc helper function', t => {
+  t.ok(isFunction(dissoc), 'is a function')
+
+  const fn = bindFunc(x => dissoc(x, {}))
+
+  t.throws(fn(undefined), err, 'throws when first arg is undefined')
+  t.throws(fn(null), err, 'throws when first arg is null')
+  t.throws(fn(false, {}), err, 'throws when first arg is false')
+  t.throws(fn(true, {}), err, 'throws when first arg is true')
+  t.throws(fn(unit, {}), err, 'throws when first arg is a function')
+  t.throws(fn({}, {}), err, 'throws when first arg is an object')
+  t.throws(fn([], {}), err, 'throws when first arg is an array')
+
+  t.end()
+})
+
+test('dissoc with Object', t => {
   const fn = bindFunc(dissoc)
 
-  const noString = /dissoc: String required for first argument/
-  t.throws(fn(undefined, {}), noString, 'throws when first arg is undefined')
-  t.throws(fn(null, {}), noString, 'throws when first arg is null')
-  t.throws(fn(0, {}), noString, 'throws when first arg is falsey number')
-  t.throws(fn(1, {}), noString, 'throws when first arg is truthy number')
-  t.throws(fn(false, {}), noString, 'throws when first arg is false')
-  t.throws(fn(true, {}), noString, 'throws when first arg is true')
-  t.throws(fn(unit, {}), noString, 'throws when first arg is a function')
-  t.throws(fn({}, {}), noString, 'throws when first arg is an object')
-  t.throws(fn([], {}), noString, 'throws when first arg is an array')
+  t.throws(fn('', {}), err, 'throws when first arg is empty string')
 
-  const noObj = /dissoc: Object required for second argument/
-  t.throws(fn('', undefined), noObj, 'throws when second arg is undefined')
-  t.throws(fn('', null), noObj, 'throws when second arg is null')
-  t.throws(fn('', 0), noObj, 'throws when second arg is falsey number')
-  t.throws(fn('', 1), noObj, 'throws when second arg is truthy number')
-  t.throws(fn('', ''), noObj, 'throws when second arg is falsey string')
-  t.throws(fn('', 'string'), noObj, 'throws when second arg is truthy string')
-  t.throws(fn('', false), noObj, 'throws when second arg is false')
-  t.throws(fn('', true), noObj, 'throws when second arg is true')
+  const data =
+    { a: true }
+
+  const noString =
+    x => dissoc(x, data)
+
+  t.equals(noString(0), data, 'does nothing with falsy number as first argument')
+  t.equals(noString(1), data, 'does nothing with truthy number as first argument')
 
   const undefs = { a: undefined, b: undefined }
-  t.same(dissoc('', undefs), {}, 'removes undefined values')
+  t.same(dissoc('key', undefs), {}, 'removes undefined values')
 
   const defs = { a: 1, b: 2 }
-  t.same(dissoc('', defs), defs, 'empty string returns with nothing removed')
-  t.notEqual(dissoc('', defs), defs, 'returns a new object')
+  t.notEqual(dissoc('key', defs), defs, 'returns a new object')
   t.same(dissoc('a', defs), { b: 2 }, 'returns a new object with specified key removed')
+
+  t.end()
+})
+
+test('dissoc with Array', t => {
+  const fn = bindFunc(dissoc)
+
+  t.throws(fn(3.1416, []), err, 'throws when first arg is float')
+  t.throws(fn(-6, []), err, 'throws when first arg is negative')
+
+  const data =
+    [ 1, 2, 3 ]
+
+  const noInteger =
+    x => dissoc(x, data)
+
+  t.equals(noInteger('string'), data, 'does nothing with truthy string as first argument')
+
+  const undefs = [ undefined, undefined ]
+  t.same(dissoc(0, undefs), [ undefined ], 'does not remove undefined values')
+
+  const defs = [ 34, 99, 'string' ]
+  t.same(dissoc(3, defs), defs, 'keeps items when index does not exist')
+  t.notEqual(dissoc(3, defs), defs, 'returns a new object')
+
+  t.same(dissoc(1, defs), [ 34, 'string' ], 'removes item at index without holes')
+
+  t.end()
+})
+
+test('dissoc without Object or Array', t => {
+  t.equals(dissoc('a', undefined), undefined, 'returns undefined with undefined')
+  t.equals(dissoc('a', null), null, 'returns null with when null')
+  t.ok(Number.isNaN(dissoc('a', NaN)), 'returns NaN with NaN')
+  t.equals(dissoc('a', 0), 0, 'returns falsy number with falsy number')
+  t.equals(dissoc('a', 1), 1, 'returns truthy number with truthy number')
+  t.equals(dissoc('a', ''), '', 'returns falsy string with falsy string')
+  t.equals(dissoc('a', 'string'), 'string', 'returns truthy string with truthy string')
+  t.equals(dissoc('a', false), false, 'returns false with false')
+  t.equals(dissoc('a', true), true, 'returns true with true')
 
   t.end()
 })

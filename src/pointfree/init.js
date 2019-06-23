8e9426f8340a1c2c3fc65c0da7766a982aec2313
Richard Forrester <forrester.rich@gmail.com>
2019-06-23 04:53:26 -1000

    correct copyright date

--- src/pointfree/init.js
+++ src/pointfree/init.js
@@ -1,4 +1,4 @@
-/** @license ISC License (c) copyright 2016 original and current authors */
+/** @license ISC License (c) copyright 2019 original and current authors */
 /** @author RichardForrester */
 
 const isFunction = require('../core/isFunction')
